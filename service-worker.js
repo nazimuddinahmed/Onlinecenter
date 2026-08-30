68bd527ba7467a6fb7b6ae9c1a5c606316086911
nazimuddinahmed <nazimuddinahmed@ymail.com>
2026-08-30 17:41:01 +0530

    Rename cache and update caching logic in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,23 @@
-const CACHE_NAME = "online-center-v1";
+const CACHE_NAME = "rezaul-online-v1";
 
-const FILES_TO_CACHE = [
+const filesToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(filesToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
